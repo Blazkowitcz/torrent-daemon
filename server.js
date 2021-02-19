efe8a4ce4dd9e4ddb3cb7fc79599a875f1d2f9c6
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-02-19 16:29:20 +0100

    add the client in main app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
+const client_torrent = require('./app/torrent-client');
+const conf = require('./client_conf.json')
 const app = express();
 
-app.listen(3000, function() {
-
+/**
+ * App init
+ */
+app.listen(conf.port, function() {
+    client_torrent.init();
 })

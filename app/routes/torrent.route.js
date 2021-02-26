f0d3bc8272e7c3021681dc486b8a9719fc98a148
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-02-26 22:07:46 +0100

    clean route

--- app/routes/torrent.route.js
+++ app/routes/torrent.route.js
@@ -1,7 +1,16 @@
 module.exports = app => {
     const torrent = require('../controllers/torrent.controller');
+
+    /**
+     * ROUTE POST
+     */
     app.post('/pause-torrent', torrent.pauseTorrent);
     app.post('/resume-torrent', torrent.resumeTorrent);
     app.post('/add-torrent', torrent.addTorrent);
+
+    /**
+     * ROUTE GET
+     */
     app.get('/torrents', torrent.getTorrents);
+    app.get('/torrents-short', torrent.getTorrentsShortData);
 }

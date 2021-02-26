64d72afbbe040c8f7545bdfb98474ea059af3944
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-02-26 15:43:40 +0100

    Project cleaning

--- app/routes/client.route.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-    const client = require('../controllers/client.controller');
-    app.post('/add-torrent', client.addTorrent);
-    app.get('/torrents', client.getTorrents);
-}
--- app/routes/torrent.route.js
+++ app/routes/torrent.route.js
@@ -2,4 +2,6 @@ module.exports = app => {
     const torrent = require('../controllers/torrent.controller');
     app.post('/pause-torrent', torrent.pauseTorrent);
     app.post('/resume-torrent', torrent.resumeTorrent);
+    app.post('/add-torrent', torrent.addTorrent);
+    app.get('/torrents', torrent.getTorrents);
 }

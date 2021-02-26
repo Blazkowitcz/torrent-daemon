5aa7633f73d47698b57b06eb9b5be85b8b455f6b
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-02-26 22:07:17 +0100

    Transfom bytes size into text more readable

--- app/utils/torrent.utils.js
+++ app/utils/torrent.utils.js
@@ -18,4 +18,18 @@ function getStatus(torrent){
     }
 }
 
-module.exports = { getStatus }
+function sizeReadable(bytes, speed) {
+    var i = -1;
+    var byteUnits = [' Kb', ' Mb', ' Gb', ' Tb'];
+    do {
+        bytes = bytes / 1024;
+        i++;
+    } while (bytes > 1024);
+    result = Math.max(bytes, 0).toFixed(1) + byteUnits[i];
+    if(speed === true){
+        result += '/s';
+    }
+    return result;
+}
+
+module.exports = { getStatus, sizeReadable }
